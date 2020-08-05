7094c76f65c49b20386b334b29345a8ef84d3251
Adrian Celczyński <adriancelczynski@icloud.com>
2020-08-05 20:31:27 +0200

    Add missing types

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,7 +1,7 @@
-export const debounce = (callback, wait): EventListener => {
+export const debounce = (callback: () => void, wait: number): EventListener => {
     let timeout;
 
-    return (...args): void => {
+    return (...args: unknown[]): void => {
         clearTimeout(timeout);
         timeout = setTimeout(() => callback.apply(this, args), wait);
     };
